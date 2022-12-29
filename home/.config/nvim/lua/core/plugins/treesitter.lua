6a0cde53ce3eac01aac1d6ac7cc55d539cf0a888
matthis-k <matthis.kaelble@gmail.com>
2022-12-29 10:20:09 +0100

    Fix: Error message that sometimes appeared

--- home/.config/nvim/lua/core/plugins/treesitter.lua
+++ home/.config/nvim/lua/core/plugins/treesitter.lua
@@ -1,6 +1,13 @@
 local function config()
+    local parsers = require("nvim-treesitter.parsers").available_parsers()
+    for i, parser in ipairs(parsers) do
+        if not parser.maintainer then
+            table.remove(parsers, i)
+        end
+    end
+
     require("nvim-treesitter.configs").setup({
-        ensure_installed = { "all" },
+        ensure_installed = parsers,
         ignore_install = { "javascript" },
     })
 end
